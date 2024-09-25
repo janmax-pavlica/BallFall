595b3aaef82a5c695ad67d22d7e8ddb5b310a059
Jan Max Pavlica <janmax.pavlica@techfides.cz>
2024-09-25 22:38:09 +0200

    wip

--- objects/ramp_half_speed_up.gd
+++ objects/ramp_half_speed_up.gd
@@ -1,12 +1,16 @@
 extends "res://scripts/ramp_parts.gd"
 
 var speeding = false
-var speedingValue = 100
+var speedingValue = 10000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if speeding:
-		Global.Ball.apply_central_impulse(Vector2(speedingValue,0))
+		var velocity = Global.Ball.get_linear_velocity()
+		
+		Global.Ball.apply_central_impulse(Vector2(speedingValue * delta,speedingValue * delta))
+		#Global.Ball.apply_central_impulse(Vector2(velocity.x + speedingValue * delta,velocity.y + speedingValue * delta))
+		print(velocity, delta)
 
 
 func _on_speed_area_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:

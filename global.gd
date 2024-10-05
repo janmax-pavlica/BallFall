f0b508a113485710aa2584ce8f68c409066e9cc7
Jan Max Pavlica <janmax.pavlica@techfides.cz>
2024-10-05 11:43:52 +0200

    wip

--- global.gd
+++ global.gd
@@ -1,23 +1,31 @@
 extends Node2D
+@onready var Ball: Ball = $osBallRigid
 
-enum GameState {PREPARING=1, RUNNING=2, FAILED=3, FINISHED=4}
-var Ball: Ball
+enum GameState { PREPARING = 1, RUNNING = 2, FAILED = 3, FINISHED = 4 }
 var _CurrentGameState: GameState = GameState.PREPARING
 var isDragging = false
 
+
 func _draw() -> void:
 	for n in 12:
-		draw_line(Vector2(128,128 + 64 * n),Vector2(128 + 64 * 14,128 + 64 * n), Color.DIM_GRAY, 1) 
+		draw_line(
+			Vector2(128, 128 + 64 * n), Vector2(128 + 64 * 14, 128 + 64 * n), Color.DIM_GRAY, 1
+		)
 	for n in 14:
-		draw_line(Vector2(128 + 64 * n,128 ),Vector2(128 + 64 * n,128 + 64 * 14), Color.DIM_GRAY, 1) 
-	#draw_multiline([Vector2(128,128),Vector2(128*12,128)],Color.RED,1) 
+		draw_line(
+			Vector2(128 + 64 * n, 128), Vector2(128 + 64 * n, 128 + 64 * 14), Color.DIM_GRAY, 1
+		)
+	#draw_multiline([Vector2(128,128),Vector2(128*12,128)],Color.RED,1)
+
 
 func setState(newState: GameState):
 	_CurrentGameState = newState
 
+
 func isStateActive(state: GameState) -> bool:
 	#print("isStateActive", state, getState())
 	return getState() == state
+
 
 func getState() -> GameState:
 	return _CurrentGameState
--- scripts/ball_rigid.gd
+++ scripts/ball_rigid.gd
@@ -4,25 +4,23 @@ extends RigidBody2D
 
 var position0 = Vector2.ZERO
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.Ball = self
 	position0 = self.global_position
 
-	self.freeze = true
+	#self.freeze = true
 	print("_ready", position0)
 
 
- #Called every frame. 'delta' is the elapsed time since the previous frame.
+#Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	self.freeze = !Global.isStateActive(Global.GameState.RUNNING)
 
-func reset() -> void : 
+
+func reset() -> void:
 	print("reset", position0)
 	PhysicsServer2D.body_set_state(
-	get_rid(),
-	PhysicsServer2D.BODY_STATE_TRANSFORM,
-	Transform2D.IDENTITY.translated(position0)
-)
-	
-	
+		get_rid(), PhysicsServer2D.BODY_STATE_TRANSFORM, Transform2D.IDENTITY.translated(position0)
+	)

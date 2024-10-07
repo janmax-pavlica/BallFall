18b2492af86874b1191ba7d59fb31922ae8d63ba
Jan Max Pavlica <janmax.pavlica@techfides.cz>
2024-10-07 21:25:48 +0200

    wip

--- global.gd
+++ global.gd
@@ -5,17 +5,17 @@ enum GameState { PREPARING = 1, RUNNING = 2, FAILED = 3, FINISHED = 4 }
 var _CurrentGameState: GameState = GameState.PREPARING
 var isDragging = false
 
-
-func _draw() -> void:
-	for n in 6:
-		draw_line(
-			Vector2(128, 128 + 128 * n), Vector2(128 + 12864 * 6, 128 + 128 * n), Color.DIM_GRAY, 1
-		)
-	for n in 6:
-		draw_line(
-			Vector2(128 + 128 * n, 128), Vector2(128 + 128 * n, 128 + 128 * 6), Color.DIM_GRAY, 1
-		)
-	#draw_multiline([Vector2(128,128),Vector2(128*12,128)],Color.RED,1)
+#
+#func _draw() -> void:
+#for n in 6:
+#draw_line(
+#Vector2(128, 128 + 128 * n), Vector2(128 + 12864 * 6, 128 + 128 * n), Color.DIM_GRAY, 1
+#)
+#for n in 6:
+#draw_line(
+#Vector2(128 + 128 * n, 128), Vector2(128 + 128 * n, 128 + 128 * 6), Color.DIM_GRAY, 1
+#)
+#draw_multiline([Vector2(128,128),Vector2(128*12,128)],Color.RED,1)
 
 
 func setState(newState: GameState):
